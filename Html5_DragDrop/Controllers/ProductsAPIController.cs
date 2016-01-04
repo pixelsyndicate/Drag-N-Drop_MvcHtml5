a9800b42a131ee3b479add1c793b3df472f3a65d
pixelsyndicate@hotmail.com <pixelsyndicate@hotmail.com>
2016-01-04 14:14:57 -0600

    added data and event handling for drag of list items.

---
+++ Html5_DragDrop/Controllers/ProductsAPIController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Html5_DragDrop.Models;
+
+namespace Html5_DragDrop.Controllers
+{
+    public class ProductsAPIController : ApiController
+    {
+
+        ApplicationEntities ctx;
+
+        public ProductsAPIController()
+        {
+            ctx = new ApplicationEntities();
+        }
+
+        [Route("Products")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return ctx.Products.ToList();
+        }
+
+    }
+}
